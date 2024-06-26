881c341af6b315679a598d957379d435a8375760
Amitdhale <158050148+Amitdhale@users.noreply.github.com>
2024-06-26 17:30:03 +0530

    Update firebase.init.js

--- frontend/src/firebase.init.js
+++ frontend/src/firebase.init.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAdmhahWVNnv8bpypNsrQh5EIaUS9zaqpA",
-  authDomain: "twitter-clone-e2212.firebaseapp.com",
-  projectId: "twitter-clone-e2212",
-  storageBucket: "twitter-clone-e2212.appspot.com",
-  messagingSenderId: "383051864014",
-  appId: "1:383051864014:web:af758634e0b5747d4bbd9f",
-  measurementId: "G-3E1GV2W18H"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
